ad22394caba4820bbd7ee21457ec3cb30343a6b2
Edward A Maxedon, Sr <edadma@gmail.com>
2019-09-13 20:11:49 -0400

    cont coding parser

--- src/main/scala/AST.scala
+++ src/main/scala/AST.scala
@@ -12,20 +12,19 @@ abstract class DeclarationAST extends AST {
   val name: String
 }
 
-case class ProcedureDeclaration( pos: Reader, name: String, args: List[(Reader, String)], decls: List[DeclarationAST], stat: StatementAST ) extends DeclarationAST
-case class ConstDeclaration( pos: Reader, name: String, value: Any ) extends DeclarationAST
-case class VarDeclaration( pos: Reader, name: String, value: Option[Any] ) extends DeclarationAST
+case class FunctionDeclaration( pos: Reader, name: String, parms: List[(Reader, String)], decls: List[DeclarationAST], stat: StatementAST ) extends DeclarationAST
+case class ConstDeclaration( pos: Reader, name: String, value: ExpressionAST ) extends DeclarationAST
+case class VarDeclaration( pos: Reader, name: String, value: Option[ExpressionAST] ) extends DeclarationAST
 
 abstract class StatementAST extends AST
 case class AssignStatement( pos: Reader, name: String, expr: ExpressionAST ) extends StatementAST
-case class CallStatement( pos: Reader, name: String ) extends StatementAST
-case class WritStatemente( expr: ExpressionAST ) extends StatementAST
+case class ApplyStatement( pos: Reader, name: String, args: List[ExpressionAST] ) extends StatementAST
+case class WriteStatement( expr: ExpressionAST ) extends StatementAST
 case class SequenceStatement( stats: List[StatementAST] ) extends StatementAST
 case class IfStatement( cond: ConditionAST, stat: StatementAST ) extends StatementAST
 case class WhileStatement( cond: ConditionAST, stat: StatementAST ) extends StatementAST
 
 abstract class ConditionAST extends AST
-case class OddCondition( expr: ExpressionAST ) extends ConditionAST
 case class ComparisonCondition( left: ExpressionAST, comp: String, right: ExpressionAST ) extends ConditionAST
 
 abstract class ExpressionAST extends AST
--- src/main/scala/Parser.scala
+++ src/main/scala/Parser.scala
@@ -10,8 +10,8 @@ object Parser extends Matchers[Reader] {
 
   def program = matchall( programBlock )
 
-  def const = pos ~ ident ~ "=" ~ integerLit ^^ {
-    case p ~ n ~ _ ~ v => n -> (p, v)
+  def const = pos ~ ident ~ "=" ~ expression ^^ {
+    case p ~ n ~ _ ~ e => ConstDeclaration( p, n, e )
   }
 
   def consts = opt("const" ~> rep1sep(const, ",") <~ ";") ^^ {
@@ -19,9 +19,8 @@ object Parser extends Matchers[Reader] {
     case Some( l ) => l
   }
 
-  def vari = pos ~ ident ~ opt("=" ~> integerLit) ^^ {
-    case p ~ n ~ None => n -> (p, new Var( 0 ))
-    case p ~ n ~ Some( v ) => n -> (p, new Var( v ))
+  def vari = pos ~ ident ~ opt("=" ~> expression) ^^ {
+    case p ~ n ~ e => VarDeclaration( p, n, e )
   }
 
   def vars = opt("var" ~> rep1sep(vari, ",") <~ ";") ^^ {
@@ -29,39 +28,42 @@ object Parser extends Matchers[Reader] {
     case Some( l ) => l
   }
 
-  def proc: Matcher[ProcedureDeclaration] = "def" ~ pos ~ ident ~ ";" ~ block ~ ";" ^^ {
-    case _ ~ p ~ n ~ _ ~ b ~ _ => ProcedureDeclaration( p, n, Nil, b )
+  def parms =
+    repsep(pos ~ ident, ",") ^^ (_ map { case p ~ i => (p, i) })
+
+  def function: Matcher[FunctionDeclaration] = ("def" ~> pos) ~ ident ~ ("(" ~> parms <~ ")") ~ ("=" ~> statement) ^^ {
+    case p ~ n ~ ps ~ s => FunctionDeclaration( p, n, ps, Nil, s )
   }
 
-  def programBlock = consts ~ vars ~ rep(proc) ~ statement ^^ {
+  def programBlock = consts ~ vars ~ rep(function) ~ statement ^^ {
     case c ~ v ~ p ~ s => Program( c ++ v ++ p, s )
   }
 
   def statement: Matcher[StatementAST] =
-    pos ~ ident ~ ":=" ~ expression ^^ { case p ~ n ~ _ ~ e => Assign( p, n, e ) } |
-      "call" ~ pos ~ ident ^^ { case _ ~ p ~ n => Call( p, n ) } |
-      "!" ~> expression ^^ Write |
-      "begin" ~> rep1sep(statement, ";") <~ "end" ^^ Sequence |
-      "if" ~ condition ~ "then" ~ statement ^^ { case _ ~ c ~ _ ~ s => If( c, s ) } |
-      "while" ~ condition ~ "do" ~ statement ^^ { case _ ~ c ~ _ ~ s => While( c, s ) }
+    pos ~ ident ~ ":=" ~ expression ^^ { case p ~ n ~ _ ~ e => AssignStatement( p, n, e ) } |
+    pos ~ ident ~ ("(" ~> repsep(expression, ",") <~ ")") ^^ {
+      case p ~ n ~ a => ApplyStatement( p, n, a ) } |
+    "!" ~> expression ^^ WriteStatement |
+    "{" ~> rep1sep(statement, ";") <~ "}" ^^ SequenceStatement |
+    "if" ~ condition ~ "then" ~ statement ^^ { case _ ~ c ~ _ ~ s => IfStatement( c, s ) } |
+    "while" ~ condition ~ "do" ~ statement ^^ { case _ ~ c ~ _ ~ s => WhileStatement( c, s ) }
 
   def condition =
-    "odd" ~> expression ^^ Odd |
-      expression ~ ("="|"#"|"<"|"<="|">"|">=") ~ expression ^^ { case l ~ c ~ r => Comparison( l, c, r ) }
+    expression ~ ("="|"#"|"<"|"<="|">"|">=") ~ expression ^^ { case l ~ c ~ r => ComparisonCondition( l, c, r ) }
 
   def expression: Matcher[ExpressionAST] = opt("+" | "-") ~ term ~ rep(("+" | "-") ~ term) ^^ {
-    case (None|Some("+")) ~ t ~ l => (l foldLeft t) { case (x, o ~ y) => Operation( x, o, y ) }
-    case _ ~ t ~ l => (l foldLeft (Negate( t ): ExpressionAST)) { case (x, o ~ y) => Operation( x, o, y ) }
+    case (None|Some("+")) ~ t ~ l => (l foldLeft t) { case (x, o ~ y) => BinaryExpression( x, o, y ) }
+    case _ ~ t ~ l => (l foldLeft (NegateExpression( t ): ExpressionAST)) { case (x, o ~ y) => BinaryExpression( x, o, y ) }
   }
 
-  def term = factor ~ rep(("*" | "/") ~ factor) ^^ {
-    case f ~ l => (l foldLeft f) { case (x, o ~ y) => Operation( x, o, y ) }
+  def term: Matcher[ExpressionAST] = factor ~ rep(("*" | "/") ~ factor) ^^ {
+    case f ~ l => (l foldLeft f) { case (x, o ~ y) => BinaryExpression( x, o, y ) }
   }
 
-  def factor =
-    pos ~ ident ^^ { case p ~ v => Ident( p, v ) } |
-      integerLit ^^ Number |
-      "(" ~> expression <~ ")"
+  def factor: Matcher[ExpressionAST] =
+    pos ~ ident ^^ { case p ~ v => IdentExpression( p, v ) } |
+    integerLit ^^ NumberExpression |
+    "(" ~> expression <~ ")"
 
   def run( s: String ) =
     program( Reader.fromString(s) ) match {
