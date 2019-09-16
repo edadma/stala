4f1c235f06c0b4beb4524ac0d03b4e89e5fce849
Edward A Maxedon, Sr <edadma@gmail.com>
2019-09-16 15:25:00 -0400

    cont coding preprocessor

--- src/main/scala/Preprocessor.scala
+++ src/main/scala/Preprocessor.scala
@@ -13,20 +13,27 @@ object Preprocessor {
         case None =>
           val consts =
             block.consts map {
-              case ConstDeclaration(pos, name, value) => name -> preprocessExpression(value, scope)
+              case c@ConstDeclaration(pos, name, value) =>
+                preprocessExpression(value, scope)
+                name -> c
             }
           val vars =
             block.vars map {
-              case VarDeclaration( pos, name, None ) => name -> Var( 0 )
-              case VarDeclaration( pos, name, Some(value) ) => name -> Var( preprocessExpression(value, scope) )
+              case v@VarDeclaration( pos, name, None, _ ) =>
+                v.value = Var( 0 )
+                name -> v
+              case VarDeclaration( pos, name, Some(value) ) =>
+                preprocessExpression(value, scope)
+                name -> Var( 0 )
             }
-          val funcs =
-            block.funcs map {
-              case FunctionDeclaration( pos,)
-            case m@MachineDeclaration( pos, name, decls, states ) =>
-              name -> m
-            case d => d.name -> d
-          }.toMap
+//          val funcs =
+//            block.funcs map {
+//              case FunctionDeclaration(pos, name, parms, stat) =>
+//            }
+//            case m@MachineDeclaration( pos, name, decls, states ) =>
+//              name -> m
+//            case d => d.name -> d
+          (consts ++ vars).toMap
         case Some( List(d) ) => sys.error( d.pos.longErrorText(s"'${d.name}' is already defined") )
       }
 
@@ -54,6 +61,11 @@ object Preprocessor {
     }
 
   def preprocessExpression( expr: ExpressionAST, scope: List[Map[String, Any]] ): Unit = {
+//    find( name, scope ) match {
+//      case None => sys.error( pos.longErrorText(s"variable '$name' not found") )
+//      case Some( v: Var ) => v.v = preprocessAndEvalExpression( expr, scope )
+//      case _ => sys.error(pos.longErrorText(s"'$name' not assignable"))
+//    }
 
   }
 
